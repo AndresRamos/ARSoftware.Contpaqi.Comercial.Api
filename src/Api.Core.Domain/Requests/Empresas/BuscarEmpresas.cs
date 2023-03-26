695fc08695526dd37e4ada034c2146def8ec578d
Andres Ramos <andres@arsoft.net>
2023-03-26 18:17:11 -0500

    docs: actualiza la documentacion de solicitudes de emrpesas

--- src/Api.Core.Domain/Requests/Empresas/BuscarEmpresas.cs
+++ src/Api.Core.Domain/Requests/Empresas/BuscarEmpresas.cs
@@ -2,29 +2,46 @@
 
 namespace Api.Core.Domain.Requests;
 
-/// <inheritdoc cref="IApiRequest" />
+/// <summary>
+///     Solicitud para buscar empresas.
+/// </summary>
 public sealed class BuscarEmpresasRequest : ApiRequestBase, IApiRequest<BuscarEmpresasRequestModel, BuscarEmpresasRequestOptions>
 {
     public BuscarEmpresasRequestModel Model { get; set; } = new();
     public BuscarEmpresasRequestOptions Options { get; set; } = new();
 }
 
+/// <summary>
+///     Modelo de la solicitud BuscarEmpresasRequest.
+/// </summary>
 public sealed class BuscarEmpresasRequestModel
 {
 }
 
+/// <summary>
+///     Opciones de la solicitud BuscarEmpresasRequest.
+/// </summary>
+/// <inheritdoc cref="ILoadRelatedDataOptions" />
 public sealed class BuscarEmpresasRequestOptions : ILoadRelatedDataOptions
 {
     public bool CargarDatosExtra { get; set; }
 }
 
-/// <inheritdoc cref="IApiResponse" />
+/// <summary>
+///     Respuesta de la solicitud BuscarEmpresasRequest.
+/// </summary>
 public sealed class BuscarEmpresasResponse : ApiResponseBase, IApiResponse<BuscarEmpresasResponseModel>
 {
     public BuscarEmpresasResponseModel Model { get; set; } = new();
 }
 
+/// <summary>
+///     Modelo de la solicitud BuscarEmpresasResponse.
+/// </summary>
 public sealed class BuscarEmpresasResponseModel
 {
+    /// <summary>
+    ///     Lista de empresas encontradas.
+    /// </summary>
     public List<Empresa> Empresas { get; set; } = new();
 }
