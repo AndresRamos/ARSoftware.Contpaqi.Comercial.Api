1d916395d7641310c69ad5ad179fe2e8683280a1
Andres Ramos <andres@arsoft.net>
2023-03-26 18:27:07 -0500

    docs: actualiza documentacion de ApiRequestBase y ApiResponseBase

--- src/Api.Core.Domain/Common/ApiRequestBase.cs
+++ src/Api.Core.Domain/Common/ApiRequestBase.cs
@@ -5,17 +5,35 @@ namespace Api.Core.Domain.Common;
 
 public abstract class ApiRequestBase : IRequest<ApiResponseBase>
 {
+    /// <summary>
+    ///     Id de la solicitud.
+    /// </summary>
     public Guid Id { get; set; }
 
+    /// <summary>
+    ///     Licencia del consumidor de la API.
+    /// </summary>
     public string SubscriptionKey { get; set; } = Guid.Empty.ToString("N");
 
+    /// <summary>
+    ///     RFC de la empresa.
+    /// </summary>
     public string EmpresaRfc { get; set; } = string.Empty;
 
+    /// <summary>
+    ///     Fecha de creacion de la solicitud.
+    /// </summary>
     public DateTime DateCreated { get; set; } = DateTime.Now;
 
+    /// <summary>
+    ///     Estatus de la solicitud.
+    /// </summary>
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public RequestStatus Status { get; set; } = RequestStatus.Pending;
 
+    /// <summary>
+    ///     Respuesta de la solicitud.
+    /// </summary>
     public ApiResponseBase? Response { get; set; }
 
     public void SetCreateDefaults(string subscriptionKey)
