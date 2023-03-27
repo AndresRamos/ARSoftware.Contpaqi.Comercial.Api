e502de89e705752d5e3c522b286d7b83e2ffb577
Andres Ramos <andres@arsoft.net>
2023-03-26 19:44:16 -0500

    refactor: add migration BuscarDocumentosRequest

--- src/Api.Infrastructure/Persistence/Migrations/20230327004328_add-BuscarDocumentosRequest.Designer.cs
+++ src/Api.Infrastructure/Persistence/Migrations/20230327004328_add-BuscarDocumentosRequest.Designer.cs
@@ -4,6 +4,7 @@ using Api.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230327004328_add-BuscarDocumentosRequest")]
+    partial class addBuscarDocumentosRequest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Api.Infrastructure/Persistence/Migrations/20230327004328_add-BuscarDocumentosRequest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBuscarDocumentosRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
