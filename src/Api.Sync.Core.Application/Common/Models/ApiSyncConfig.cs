fa1433406b254a2a2cebe163219879ffabf6843d
Andres Ramos <andres@arsoft.net>
2023-10-14 04:31:40 -0600

    Valida tiempo de apagado cuando se inicia despues del ShutdownTime

--- src/Api.Sync.Core.Application/Common/Models/ApiSyncConfig.cs
+++ src/Api.Sync.Core.Application/Common/Models/ApiSyncConfig.cs
@@ -2,18 +2,24 @@
 
 public sealed class ApiSyncConfig
 {
-    private readonly TimeOnly _timeStarted = TimeOnly.FromDateTime(DateTime.Now);
+    private readonly DateTime _timeStarted = DateTime.Now;
+
+    public ApiSyncConfig()
+    {
+        ShutdownDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, ShutdownTime.Hour, ShutdownTime.Minute,
+            ShutdownTime.Second);
+
+        if (_timeStarted > ShutdownDateTime) ShutdownDateTime = ShutdownDateTime.AddDays(1);
+    }
+
     public string SubscriptionKey { get; set; } = "00000000000000000000000000000000";
     public string BaseAddress { get; set; } = string.Empty;
     public TimeOnly ShutdownTime { get; set; } = new(20, 0, 0);
     public List<string> Empresas { get; set; } = new();
+    private DateTime ShutdownDateTime { get; }
 
     public bool ShouldShutDown()
     {
-        bool startedAfterShutdownTime = _timeStarted > ShutdownTime;
-
-        bool isPastShutdownTime = TimeOnly.FromDateTime(DateTime.Now) >= ShutdownTime;
-
-        return !startedAfterShutdownTime && isPastShutdownTime;
+        return DateTime.Now >= ShutdownDateTime;
     }
 }
