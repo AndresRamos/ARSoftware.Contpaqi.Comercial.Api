b83672731c1f36f6fd62c26a7ddd07032c58ae0d
Andres Ramos <andres@arsoft.net>
2023-12-11 06:12:19 -0600

    agrega crear direccion request handleer

---
+++ src/Api.Core.Domain/Requests/Direcciones/CreaDireccionCliente.cs
@@ -0,0 +1,67 @@
+﻿using ARSoftware.Contpaqi.Api.Common.Domain;
+
+namespace Api.Core.Domain.Requests.Direcciones;
+
+/// <summary>
+///     Solicitud para crear una dirección de un cliente.
+/// </summary>
+public sealed class CreaDireccionClienteRequest : ContpaqiRequest<CreaDireccionClienteRequestModel, CreaDireccionClienteRequestOptions,
+    CreaDireccionClienteResponse>
+{
+    public CreaDireccionClienteRequest(CreaDireccionClienteRequestModel model, CreaDireccionClienteRequestOptions options) : base(model,
+        options)
+    {
+    }
+}
+
+/// <summary>
+///     Modelo de la solicitud CreaDireccionClienteRequest.
+/// </summary>
+public sealed class CreaDireccionClienteRequestModel
+{
+    /// <summary>
+    ///     Código del cliente al que se le creara la dirección.
+    /// </summary>
+    public string CodigoCliente { get; set; }
+
+    /// <summary>
+    ///     Direccion a crear.
+    /// </summary>
+    public Direccion Direccion { get; set; } = new();
+}
+
+/// <summary>
+///     Opciones de la solicitud CreaDireccionClienteRequest.
+/// </summary>
+public sealed class CreaDireccionClienteRequestOptions : ILoadRelatedDataOptions
+{
+    public bool CargarDatosExtra { get; set; }
+}
+
+/// <summary>
+///     Respuesta de la solicitud CreaDireccionClienteRequest.
+/// </summary>
+public sealed class CreaDireccionClienteResponse : ContpaqiResponse<CreaDireccionClienteResponseModel>
+{
+    public CreaDireccionClienteResponse(CreaDireccionClienteResponseModel model) : base(model)
+    {
+    }
+
+    public static CreaDireccionClienteResponse CreateInstance(Direccion direccion)
+    {
+        return new CreaDireccionClienteResponse(new CreaDireccionClienteResponseModel(direccion));
+    }
+}
+
+/// <summary>
+///     Modelo de la respuesta CreaDireccionClienteResponse.
+/// </summary>
+public sealed class CreaDireccionClienteResponseModel
+{
+    public CreaDireccionClienteResponseModel(Direccion direccion)
+    {
+        Direccion = direccion;
+    }
+
+    public Direccion Direccion { get; }
+}
---
+++ src/Api.Sync.Core.Application/Requests/Direcciones/CrearDireccionCliente/CrearDireccionClienteRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using Api.Core.Domain.Requests.Direcciones;
+using Api.Sync.Core.Application.ContpaqiComercial.Interfaces;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Interfaces;
+using MediatR;
+
+namespace Api.Sync.Core.Application.Requests.Direcciones.CrearDireccionCliente;
+
+public sealed class CrearDireccionClienteRequestHandler : IRequestHandler<CreaDireccionClienteRequest, CreaDireccionClienteResponse>
+{
+    private readonly IDireccionRepository _direccionRepository;
+    private readonly IDireccionService _direccionService;
+
+    public CrearDireccionClienteRequestHandler(IDireccionService direccionService, IDireccionRepository direccionRepository)
+    {
+        _direccionService = direccionService;
+        _direccionRepository = direccionRepository;
+    }
+
+    public Task<CreaDireccionClienteResponse> Handle(CreaDireccionClienteRequest request, CancellationToken cancellationToken)
+    {
+        int direccionId = _direccionService.Crear(request.Model.CodigoCliente, request.Model.Direccion);
+
+        Direccion direccion = _direccionRepository.BuscarDireccionPorId(direccionId) ?? throw new InvalidOperationException();
+
+        return Task.FromResult(CreaDireccionClienteResponse.CreateInstance(direccion));
+    }
+}
