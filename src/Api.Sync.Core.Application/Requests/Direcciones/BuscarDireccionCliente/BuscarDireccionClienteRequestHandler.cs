a4b2df2bd2bfde9dfc973fe2440c2580dd862eb7
Andres Ramos <andres@arsoft.net>
2023-12-12 09:41:06 -0600

    crea handler BuscarDireccionClienteRequestHandler

---
+++ src/Api.Sync.Core.Application/Requests/Direcciones/BuscarDireccionCliente/BuscarDireccionClienteRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using Api.Core.Domain.Requests;
+using Api.Sync.Core.Application.ContpaqiComercial.Interfaces;
+using MediatR;
+
+namespace Api.Sync.Core.Application.Requests.Direcciones.BuscarDireccionCliente;
+
+public sealed class BuscarDireccionClienteRequestHandler : IRequestHandler<BuscarDireccionClienteRequest, BuscarDireccionClienteResponse>
+{
+    private readonly IDireccionRepository _direccionRepository;
+
+    public BuscarDireccionClienteRequestHandler(IDireccionRepository direccionRepository)
+    {
+        _direccionRepository = direccionRepository;
+    }
+
+    public async Task<BuscarDireccionClienteResponse> Handle(BuscarDireccionClienteRequest request, CancellationToken cancellationToken)
+    {
+        Direccion? direccion = _direccionRepository.BuscarDireccionPorCliente(request.Model.CodigoCliente, request.Model.Tipo);
+
+        return BuscarDireccionClienteResponse.CreateInstance(direccion);
+    }
+}
