00b2ba0dddff58f1c1dfbf0ac197a69f8ea475a9
Andres Ramos <andres@arsoft.net>
2023-12-12 10:42:39 -0600

    crea solicitud para actualizar unidad de medida

---
+++ src/Api.Core.Domain/Requests/UnidadesMedida/ActualizarUnidadMedida.cs
@@ -0,0 +1,57 @@
+﻿using ARSoftware.Contpaqi.Api.Common.Domain;
+
+namespace Api.Core.Domain.Requests;
+
+/// <summary>
+///     Solicitud para actualizar una unidad de medida.
+/// </summary>
+public sealed class ActualizarUnidadMedidaRequest : ContpaqiRequest<ActualizarUnidadMedidaRequestModel, ActualizarUnidadMedidaRequestOptions
+    , ActualizarUnidadMedidaResponse>
+{
+    public ActualizarUnidadMedidaRequest(ActualizarUnidadMedidaRequestModel model, ActualizarUnidadMedidaRequestOptions options) :
+        base(model, options)
+    {
+    }
+}
+
+/// <summary>
+///     Modelo de la solicitud ActualizarUnidadMedidaRequest.
+/// </summary>
+public sealed class ActualizarUnidadMedidaRequestModel
+{
+    /// <summary>
+    ///     Nombre de la unidad de medida a actualizar.
+    /// </summary>
+    public string Nombre { get; set; }
+
+    /// <summary>
+    ///     Unidad de medida a actualizar.
+    /// </summary>
+    public UnidadMedida UnidadMedida { get; set; }
+}
+
+/// <summary>
+///     Opciones de la solicitud ActualizarUnidadMedidaRequest.
+/// </summary>
+public sealed class ActualizarUnidadMedidaRequestOptions : ILoadRelatedDataOptions
+{
+    public bool CargarDatosExtra { get; set; }
+}
+
+/// <summary>
+///     Respuesta de la solicitud ActualizarUnidadMedidaRequest.
+/// </summary>
+public sealed class ActualizarUnidadMedidaResponse : ContpaqiResponse<ActualizaDireccionClienteResponseModel>
+{
+    public ActualizarUnidadMedidaResponse(ActualizaDireccionClienteResponseModel model) : base(model)
+    {
+    }
+}
+
+/// <summary>
+///     Modelo de la respuesta ActualizarUnidadMedidaResponse.
+/// </summary>
+public sealed class ActualizarUnidadMedidaResponseModel
+{
+    public UnidadMedida UnidadMedida { get; set; }
+}
