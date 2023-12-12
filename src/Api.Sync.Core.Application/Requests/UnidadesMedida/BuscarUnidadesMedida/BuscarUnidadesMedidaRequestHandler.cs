505f89a2a9588d701a3c543f2ff894fa49c1b3a0
Andres Ramos <andres@arsoft.net>
2023-12-12 11:47:43 -0600

    implementa el request handler

---
+++ src/Api.Sync.Core.Application/Requests/UnidadesMedida/BuscarUnidadesMedida/BuscarUnidadesMedidaRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using Api.Core.Domain.Requests;
+using Api.Sync.Core.Application.ContpaqiComercial.Interfaces;
+using MediatR;
+
+namespace Api.Sync.Core.Application.Requests.UnidadesMedida.BuscarUnidadesMedida;
+
+public sealed class BuscarUnidadesMedidaRequestHandler : IRequestHandler<BuscarUnidadesMedidaRequest, BuscarUnidadesMedidaResponse>
+{
+    private readonly IUnidadMedidaRepository _unidadMedidaRepository;
+
+    public BuscarUnidadesMedidaRequestHandler(IUnidadMedidaRepository unidadMedidaRepository)
+    {
+        _unidadMedidaRepository = unidadMedidaRepository;
+    }
+
+    public Task<BuscarUnidadesMedidaResponse> Handle(BuscarUnidadesMedidaRequest request, CancellationToken cancellationToken)
+    {
+        List<UnidadMedida> unidadesMedida = _unidadMedidaRepository.BuscarTodo();
+
+        return Task.FromResult(BuscarUnidadesMedidaResponse.CreateInstance(unidadesMedida));
+    }
+}
