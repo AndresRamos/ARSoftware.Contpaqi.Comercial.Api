4f382b1a2ae6a1275bd722ac38af7b43932ba596
Andres Ramos <andres@arsoft.net>
2023-12-12 12:01:45 -0600

    crea el request handler para eliminar unidades

---
+++ src/Api.Sync.Core.Application/Requests/UnidadesMedida/EliminarUnidadMedida/EliminarUnidadMedidaRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using Api.Core.Domain.Requests;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Interfaces;
+using MediatR;
+
+namespace Api.Sync.Core.Application.Requests.UnidadesMedida.EliminarUnidadMedida;
+
+public sealed class EliminarUnidadMedidaRequestHandler : IRequestHandler<EliminarUnidadMedidaRequest, EliminarUnidadMedidaResponse>
+{
+    private readonly IUnidadMedidaService _unidadMedidaService;
+
+    public EliminarUnidadMedidaRequestHandler(IUnidadMedidaService unidadMedidaService)
+    {
+        _unidadMedidaService = unidadMedidaService;
+    }
+
+    public Task<EliminarUnidadMedidaResponse> Handle(EliminarUnidadMedidaRequest request, CancellationToken cancellationToken)
+    {
+        _unidadMedidaService.Eliminar(request.Model.NombreUnidad);
+
+        return Task.FromResult(new EliminarUnidadMedidaResponse(new EliminarUnidadMedidaResponseModel()));
+    }
+}
